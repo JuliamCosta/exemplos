71d8975b0fe5acc7e489a7c03df70e31dc0bc919
JuliamCosta <juuhcosta@outlook.com>
2020-03-03 17:55:18 -0300

    Aula com index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Exemplos 2020-03-03</title>    	
+    </head>
+
+    <body>
+        
+        <div>
+            <h1>Links dos Exemplos - Aula 2020/03/03</h1>
+        </div>
+        <a href="20200303/exemplo1.html">Exemplo 1</a>
+        <a href="20200303/exemplo2.html">Exemplo 2</a>
+        <a href="20200303/exemplo3.html">Exemplo 3 - script interno</a>
+        <a href="20200303/exemplo3a.html">Exemplo 3 - script externo</a>
+    </body>
+</html>
