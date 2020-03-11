530dcecf62d1a376e990ed6ad68613a03c184d3e
JuliamCosta <juuhcosta@outlook.com>
2020-03-11 17:58:44 -0300

    Exemplos

--- index.php
+++ index.php
@@ -14,5 +14,14 @@
         <a href="20200303/exemplo2.html">Exemplo 2</a>
         <a href="20200303/exemplo3.html">Exemplo 3 - script interno</a>
         <a href="20200303/exemplo3a.html">Exemplo 3 - script externo</a>
+
+        <a href="20200310/exe_onblur.html">Exemplo - On Blur</a>
+        <a href="20200310/exe_onchange.html">Exemplo - On Change</a>
+        <a href="20200310/exe_onclick.html">Exemplo - On Click</a>
+        <a href="20200310/exe_onfocus.html">Exemplo - On Focus</a>
+        <a href="20200310/exe_onload.html">Exemplo - On Load</a>
+        <a href="20200310/exe_onkeyup_onkeydown.html">Exemplo - On Key Up & On Key Down</a>
+        <a href="20200310/exe_onmouseover_out.html">Exemplo - On Mouse Over & On Mouse Out</a>
+
     </body>
 </html>
